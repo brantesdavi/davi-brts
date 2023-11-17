4249f3e696b9cf0dcbbe3c22cf9112140f0ea328
brantesdavi <davi.brantes@gmail.com>
2023-11-17 11:37:10 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        deepPink: '#EE3496',
+        gold: '#FDD52D',
+        onix: '#474445',
+        antiWhite: '#EDEDED',
+        raisinBlack: '#252223'
+      },
+      spacing:{
+        '58': '58px',
+        '54': '54px',
+        '40': '40px'
+      },
+      height:{
+        '180': '180px',
+        '140': '140px'
+      },
+      screens:{
+        'xs': '415px',
+      },
+    },
+  plugins: [],
+  }
+}
+
